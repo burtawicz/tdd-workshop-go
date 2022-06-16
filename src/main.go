7f6c92c0f74716b09ad84ce444a892d5b8fe3e63
Rick Burta <rick@rickburta.ca>
2022-06-15 21:34:33 -0400

    Add UUID library to project

--- src/main.go
+++ src/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func initializeRouter() *gin.Engine {
+	router := gin.Default()
+
+	fmt.Println("Registering mutate route...")
+	router.POST("/mutate", Mutate)
+
+	fmt.Println("Registering find route...")
+	router.POST("/find", Find)
+
+	return router
+}
 
 func main() {
-	fmt.Println("Wow, a project...")
+	fmt.Println("Preparing service...")
+	router := initializeRouter()
+
+	fmt.Println("Starting service...")
+	router.Run()
 }
